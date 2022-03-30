728ba46c5bb2f84df2f4baf402148e6a1236c4ba
Leon Si <contact@leonzalion.com>
2022-03-30 16:45:17 -0400

    fix: expose the functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-export {};
+export { toggleCheckbox } from './utils/checkbox.js';
+export {
+	clickElement,
+	createElementReference,
+	createElementReferences,
+	getElements,
+} from './utils/element.js';
+export { runAppleScript } from './utils/run.js';
+export {
+	allowSystemSoftware,
+	giveAppPermissionAccess,
+	openSystemPreferencesPane,
+	reopenSystemPreferences,
+} from './utils/system-preferences.js';
+export {
+	waitForElementExists,
+	waitForElementHidden,
+	waitForWindow,
+} from './utils/window.js';
